a7f649a4987bd7b178f17341a15d82d41af8c893
Lochlan Bernard <lochlanbernard@gmail.com>
2021-04-29 11:54:23 +0900

    working on mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React, {useState} from 'react';
-import './App.css';
-import {Card} from "./Card";
+import React, { useState } from "react";
+import "./App.css";
+import { Card } from "./Card";
+
+
 
 const App: React.FC<{}> = () => {
-  const [apiData, setApiData] = useState("");
+  const [apiData, setApiData] = useState(Object);
+
   const fetchData = () => {
-    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-    .then((response) => response.json())
-    .then((y) => setApiData(y))
-  }
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+      .then((response) => response.json())
+      .then((y) => setApiData(y));
+  };
 
   return (
-<div>
-  <button onClick={fetchData}>Fetch Api Data</button>
-  <p>{apiData !== "" && <Card name={JSON.stringify(apiData)} />}</p>
-    
-</div>
+    <div>
+      <button onClick={fetchData}>Fetch Api Data</button>
+      <p>{apiData !== "" ? <Card data={apiData} /> : <p> Here </p>}</p>
+      
+      </div>
   );
-}
-
+};
 
 export default App;
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
 interface Props{
-    name: string;
+    data: any;
+
 }
-export const Card: React.FC<Props> = ({name}) => {
+export const Card: React.FC<Props> = ({data}) => {
     
+   
     
-    
-    
-    return (
+
+  
+    return(
         <div>
-         <p>{name}</p>   
-        </div>
+         <p>{data.name}</p>   
+         <p>{data.height}</p>
+         {/* <img src={sprites} alt="sprite"></img> */}
+         <p> {(Object.values(data).map((x) => <p>{JSON.stringify(x)}</p>))}</p>
+             </div>
     )
 }
