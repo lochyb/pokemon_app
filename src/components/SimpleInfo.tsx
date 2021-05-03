d1b2669ae367be9578f99c0b73b55ade72530193
lochyb <lochyb@users.noreply.github.com>
2021-05-03 20:41:32 +0900

    refactoring

--- src/components/SimpleInfo.tsx
+++ src/components/SimpleInfo.tsx
@@ -11,33 +11,28 @@ const SimpleInfo: React.FC<Props> = ({ data }) => {
     setActive(!active);
   }
 
- 
-
   return (
-    <div className="cardContainer">
+    <div className='cardContainer'>
       <h3>Shiny List</h3>
       <p>Click to toggle Shiny</p>
       {data.map((x: any) => (
         <div
-          className="pokemonCard"
+          className='pokemonCard'
           id={`${x.id}`}
-          onMouseDown={() => toggleMouseDown()}
-                 >
-          <div className="photoWrapper">
+          onMouseDown={() => toggleMouseDown()}>
+          <div className='photoWrapper'>
             <img
               className={active ? "shiny sprites" : "sprites"}
               src={x.sprites.versions["generation-iii"].emerald.front_shiny}
-              alt="pokemonIcon"
-            ></img>
+              alt='pokemonIcon'></img>
             <img
               src={x.sprites.versions["generation-iii"].emerald.front_default}
-              alt="pokemonIcon"
-              className="sprites"
-            ></img>
+              alt='pokemonIcon'
+              className='sprites'></img>
           </div>
           <p>{x.id}</p>
           <p>{x.name}</p>
-          <div className="types">
+          <div className='types'>
             {x.types.map((x: any) => (
               <p>{x.type.name}</p>
             ))}
