d806f04778352c32e8d0d6f33e9f64a6d8f80701
Lochlan Bernard <lochlanbernard@gmail.com>
2021-06-05 16:57:02 +0900

    add tabs

--- src/components/Front.tsx
+++ src/components/Front.tsx
@@ -5,6 +5,7 @@ import { getPokemon } from "../fetch";
 import { SkeletonCard } from "./skeleton/SkeletonCard";
 
 const Front = () => {
+  const [tab, setTab] = useState(true);
   const [singlePokemon, setSinglePokemon] = useState({});
   const [singleLoading, setSingleLoading] = useState(true);
   const [manyPokemon, setManyPokemon] = useState({});
@@ -17,14 +18,14 @@ const Front = () => {
 
   async function getData(url: any) {
     setManyLoading(true);
-    console.log(manyLoading);
     const response: any = await getPokemon(url);
     const set = await loadingPokemon(response.results);
     const update = await setManyLoading(false);
   }
   useEffect(() => {
+    console.log(tab);
     return () => {};
-  }, [manyPokemon]);
+  }, [tab]);
 
   const loadingPokemon = async (data: any) => {
     let _pokemonData = await Promise.all(
@@ -36,8 +37,6 @@ const Front = () => {
     setManyPokemon(_pokemonData);
   };
 
-  // Testing Function Here
-
   function getSinglePokemon(e: any) {
     e.preventDefault();
     getPokemon(`https://pokeapi.co/api/v2/pokemon/${pokemonSearch}`)
@@ -45,50 +44,64 @@ const Front = () => {
       .then(() => setSingleLoading(false));
   }
   const test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-  // Testing Function Ends
 
   return (
     <div>
-      <h1>Pokemon Search App</h1>
-      <form onSubmit={getSinglePokemon}>
-        <input
-          type='text'
-          placeholder='Pokemon Name or Number'
-          onChange={(x) => setPokemonSearch(x.target.value.toLowerCase())}
-          required={true}
-        />
-        <button>Search</button>
-      </form>
-      <div>{!singleLoading && <Info data={singlePokemon} />}</div>
-      <hr></hr>
-      {
-        <div>
-          <button
-            onClick={() =>
-              getData("https://pokeapi.co/api/v2/pokemon?limit=151")
-            }>
-            Gen 1
-          </button>
-          <button
-            onClick={() =>
-              getData("https://pokeapi.co/api/v2/pokemon?limit=100&offset=151")
-            }>
-            Gen 2
-          </button>
+      <div className='heading-section'>
+        <h1>Pokemon Search App</h1>
+        <button
+          className='tab-button'
+          style={tab ? { background: "pink" } : { background: "white" }}
+          onClick={() => setTab(true)}>
+          <h3>Shiny List</h3>
+        </button>
+        <button
+          className='tab-button'
+          style={tab ? { background: "white" } : { background: "pink" }}
+          onClick={() => setTab(false)}>
+          <h3>Search</h3>
+        </button>
+      </div>
+      {tab ? (
+        <div className='shiny-list-wrapper'>
+          <div className='shiny-list-header'>
+            <button
+              className='btn'
+              onClick={() =>
+                getData("https://pokeapi.co/api/v2/pokemon?limit=151")
+              }>
+              Generation 1
+            </button>
+            <button
+              className='btn'
+              onClick={() =>
+                getData(
+                  "https://pokeapi.co/api/v2/pokemon?limit=100&offset=151"
+                )
+              }>
+              Generation 2
+            </button>
 
-          <button
-            onClick={() =>
-              getData("https://pokeapi.co/api/v2/pokemon?limit=135&offset=251")
-            }>
-            Gen 3
-          </button>
-          <button
-            onClick={() =>
-              getData("https://pokeapi.co/api/v2/pokemon?limit=135&offset=251")
-            }>
-            Gen 4
-          </button>
-
+            <button
+              className='btn'
+              onClick={() =>
+                getData(
+                  "https://pokeapi.co/api/v2/pokemon?limit=135&offset=251"
+                )
+              }>
+              Generation 3
+            </button>
+            <button
+              className='btn'
+              onClick={() =>
+                getData(
+                  "https://pokeapi.co/api/v2/pokemon?limit=135&offset=251"
+                )
+              }>
+              Generation 4
+            </button>
+            <h3>Click to see shiny</h3>
+          </div>
           {!manyLoading ? (
             <ShinyList data={manyPokemon} />
           ) : (
@@ -101,8 +114,20 @@ const Front = () => {
             </div>
           )}
         </div>
-      }
-      );
+      ) : (
+        <div>
+          <form onSubmit={getSinglePokemon}>
+            <input
+              type='text'
+              placeholder='Pokemon Name or Number'
+              onChange={(x) => setPokemonSearch(x.target.value.toLowerCase())}
+              required={true}
+            />
+            <button>Search</button>
+          </form>
+          <div>{!singleLoading && <Info data={singlePokemon} />}</div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/ShinyListLayout.tsx
+++ src/components/ShinyListLayout.tsx
@@ -6,47 +6,37 @@ interface Props {
 
 const ShinyPokemonToggle: React.FC<Props> = ({ data }) => {
   const [active, setActive] = useState(false);
-  const [show, setShow] = useState(true);
   return (
     <div className='cardContainer'>
-      <h3>Shiny List</h3>
-      <button onClick={() => setShow(!show)}>
-        {show ? <span>Hide</span> : <span>Show</span>}
-      </button>
-      {show && (
-        <div>
-          <p>Click to toggle Shiny</p>
-          {data.map((x: any) => (
-            <div
-              className='pokemonCard'
-              id={`${x.id}`}
-              onMouseDown={() => setActive(!active)}>
-              <div className='photoWrapper'>
-                <img
-                  className={active ? "shiny sprites" : "sprites"}
-                  src={x.sprites.versions["generation-iii"].emerald.front_shiny}
-                  alt='pokemonIcon'></img>
-                <img
-                  src={
-                    x.sprites.versions["generation-iii"].emerald.front_default
-                  }
-                  alt='pokemonIcon'
-                  className='sprites nonShiny'></img>
-              </div>
-              <p>{x.id}</p>
-              <p>{x.name}</p>
-              <div className='types'>
-                {x.types.map((x: any) => (
-                  <img
-                    className='types'
-                    src='images/Type_Normal.webp'
-                    alt={x.type.name}></img>
-                ))}
-              </div>
+      <div>
+        {data.map((x: any) => (
+          <div
+            className='pokemonCard'
+            id={`${x.id}`}
+            onMouseDown={() => setActive(!active)}>
+            <div className='photoWrapper'>
+              <img
+                className={active ? "shiny sprites" : "sprites"}
+                src={x.sprites.versions["generation-iii"].emerald.front_shiny}
+                alt='pokemonIcon'></img>
+              <img
+                src={x.sprites.versions["generation-iii"].emerald.front_default}
+                alt='pokemonIcon'
+                className='sprites nonShiny'></img>
             </div>
-          ))}
-        </div>
-      )}
+            <p>{x.id}</p>
+            <p>{x.name}</p>
+            <div className='types'>
+              {x.types.map((x: any) => (
+                <img
+                  className='types'
+                  src='images/Type_Normal.webp'
+                  alt={x.type.name}></img>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
